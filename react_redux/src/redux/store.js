356d20dd46ab94f795af92e20ff5c50359dd8313
ZHAISHENKING <18700790825@163.com>
2019-10-22 17:20:22 +0800

    react jsx语法

--- react_redux/src/redux/store.js
+++ react_redux/src/redux/store.js
@@ -1,11 +1,11 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-
+import {composeWithDevTools} from 'redux-devtools-extension'
 import {counter} from './reducers'
 
 const store = createStore(
     counter,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store
